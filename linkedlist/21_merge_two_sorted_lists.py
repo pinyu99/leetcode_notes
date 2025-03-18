a8941b34edf4ba0c269455374736c776bc71d313
clare <fdsa123j@gmail.com>
2025-03-18 22:13:44 +0800

    add a recursive solution 21_merge_two_sorted_lists

--- linkedlist/21_merge_two_sorted_lists.py
+++ linkedlist/21_merge_two_sorted_lists.py
@@ -9,6 +9,7 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+# iteration 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -43,3 +44,57 @@ class Solution(object):
         return dummy_head.next
 
 
+# recursive
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+        
+# test case 
+# list1 = [1,3,5] 
+# list2 = [2,4,6]
+
+# (第一輪)
+# mergetwolist(1->3->5, 2->4->6)
+# 1 < 2 所以 1 作為head
+# 1.next = mergetwolist (3->5, 2->4->6)
+
+# (第二輪)
+# mergetwolist(3->5, 2->4->6)
+# 2 < 3 所以 2 作為head
+# 2.next = mergetwolist (3->5, 4->6)
+
+# (第三輪)
+# mergetwolist(3->5, 4->6)
+# 3 < 4 所以 3 作為head
+# 3.next = mergetwolist (5, 4->6)
+
+# (第四輪)
+# mergetwolist(5, 4->6)
+# 4 < 5 所以 4 作為head
+# 4.next = mergetwolist (5, 6)
+
+# (第五輪)
+# mergetwolist(5, 6)
+# 5 < 6 所以 5 作為head
+# 5.next = mergetwolist (none, 6)
+
+# (第六輪)
+# mergetwolist (none, 6)
+# list1 is none
+# return list2 (equal to 6)
